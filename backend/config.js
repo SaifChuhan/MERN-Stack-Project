1b9fc3894f38a20f11b3e67100225f553221277d
Saif-ur-Rehman <moosechuhan29@gmail.com>
2025-04-14 11:47:16 +0500

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://root:12345@book-store.dnn5do0.mongodb.net/';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
